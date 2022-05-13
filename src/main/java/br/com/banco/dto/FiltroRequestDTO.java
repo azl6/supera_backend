ed56dd0cc30fc9fdddbc85d91fa102ae03148db4
Alex <alex.rodrigues23997@gmail.com>
2022-05-13 12:53:27 -0300

    Ajustes na filtragem

--- src/main/java/br/com/banco/dto/FiltroRequestDTO.java
+++ src/main/java/br/com/banco/dto/FiltroRequestDTO.java
@@ -1,20 +1,20 @@
 package br.com.banco.dto;
 
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.lang.Nullable;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
 public class FiltroRequestDTO {
 
     @Pattern(regexp = "^\\d{2}\\-\\d{2}\\-\\d{4}\\s*(?:\\d{2}:\\d{2}(?::\\d{2})?)?$", message = "O campo \"dataInicio\" deve seguir o seguinte padrão: dd-MM-yyyy, , tendo os campos HH:mm:ss como opcionais.")
-    @NotBlank(message = "O campo \"dataInicio\" não pode ser vazio.")
     private String dataInicio;
 
-    @DateTimeFormat()
     @Pattern(regexp = "^\\d{2}\\-\\d{2}\\-\\d{4}\\s*(?:\\d{2}:\\d{2}(?::\\d{2})?)?$", message = "O campo \"dataFim\" deve seguir o seguinte padrão: dd-MM-yyyy, tendo os campos HH:mm:ss como opcionais.")
-    @NotBlank(message = "O campo \"dataFim\" não pode ser vazio.")
     private String dataFim;
+
     private String nomeOperador;
 
     public FiltroRequestDTO(String dataInicio, String dataFim, String nomeOperador) {
